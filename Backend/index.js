7dbbedcdba4f6d394cadaa068ae7e0e71ee10091
dev-code-create <ayushkankale04@gmail.com>
2025-03-25 02:49:12 +0530

    cloudinary done

--- Backend/index.js
+++ Backend/index.js
@@ -1,11 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import { connectDB } from "./src/lib/db.js";
+import cookieParser from "cookie-parser";
+import authRoutes from "./src/routes/auth.route.js";
 
 const app = express();
 dotenv.config();
 
 app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => {
