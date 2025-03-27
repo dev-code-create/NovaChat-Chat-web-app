34a10d47c6be47c01b8b6ebb62be87c57f33d285
dev-code-create <ayushkankale04@gmail.com>
2025-03-27 16:13:37 +0530

    zustand and pages setup

--- Backend/index.js
+++ Backend/index.js
@@ -4,12 +4,19 @@ import { connectDB } from "./src/lib/db.js";
 import cookieParser from "cookie-parser";
 import authRoutes from "./src/routes/auth.route.js";
 import messageRoutes from "./src/routes/message.route.js";
+import cors from "cors";
 
 const app = express();
 dotenv.config();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);
