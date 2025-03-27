d9aad6b564ada1057242869ab429745be4b000df
dev-code-create <ayushkankale04@gmail.com>
2025-03-27 20:08:11 +0530

    render

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.BACKEND_URL,
+  baseURL:
+    import.meta.env.MODE === "development"
+      ? "http://localhost:5001/api"
+      : "/api",
   withCredentials: true,
 });
